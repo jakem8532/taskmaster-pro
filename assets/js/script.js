a7a1cc42df9c8a4f5606d90f289ffc2f01633b69
jakem8532 <85272755+jakem8532@users.noreply.github.com>
2022-06-01 17:23:52 -0400

    added drag and drop functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -112,6 +112,48 @@ $(".list-group").on("blur", "input[type='text']", function() {
 })
 
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  update: function(event) {
+    var tempArr = []
+
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim()
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim()
+      
+      tempArr.push({
+        text: text,
+        date: date
+      })
+    })
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "")
+    
+    tasks[arrName] = tempArr
+    saveTasks()
+  },
+
+})
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove()
+  },
+
+})
+
 
 
 // modal was triggered
