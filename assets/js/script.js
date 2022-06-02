170e9b31455a0305c26cbbef723060aadf51441c
jakem8532 <85272755+jakem8532@users.noreply.github.com>
2022-06-01 22:44:38 -0400

    added color functionality to due dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  auditTask(taskLi)
+
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -87,10 +89,16 @@ $(".list-group").on("click", "span", function() {
     .val(date)
   
   $(this).replaceWith(dateInput)
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      $(this).trigger("change")
+    }
+  })
   dateInput.trigger("focus")
 })
 
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   var date = $(this)
     .val()
     .trim()
@@ -109,6 +117,8 @@ $(".list-group").on("blur", "input[type='text']", function() {
     .addClass("bade badge-primary badge-pill")
     .text(date)
   $(this).replaceWith(taskSpan)
+
+  auditTask($(taskSpan).closest("list-group-item"))
 })
 
 
@@ -153,6 +163,24 @@ $("#trash").droppable({
   },
 
 })
+
+$("#modalDueDate").datepicker({
+  minDate: 1
+})
+
+var auditTask = function(taskEl) {
+  var date = $(taskEl).find("span").text().trim()
+
+  var time = moment(date, "L").set("hour", 17)
+
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger")
+
+  if(moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger")
+  }else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning")
+  }
+}
 
 
 
